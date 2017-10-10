ef0eecdc82c2aed5904442db3f67b2655ecf330a
Charles Dunn <charlesdunn@Charless-MBP.home>
2017-10-10 18:53:20 -0400

    initial commit

---
+++ app/data/friend.js
@@ -0,0 +1,37 @@
+//This array will hold all of my potential friends.
+//That sentence was depressing if you think about it too much
+var friendArray = [
+	{
+	  "name":"friend1",
+	  "photo":"http://via.placeholder.com/400x400",
+	  "scores":[4,5,4,3,4,5,4,3,1,1]
+	},
+	{
+	  "name":"friend2",
+	  "photo":"http://via.placeholder.com/400x400",
+	  "scores":[5,4,3,2,1,1,3,3,4,3]
+	},
+	{
+	  "name":"friend3",
+	  "photo":"http://via.placeholder.com/400x400",
+	  "scores":[1,2,3,4,2,3,4,3,2,4]
+	},
+	{
+	  "name":"friend1",
+	  "photo":"http://via.placeholder.com/400x400",
+	  "scores":[1,3,4,5,2,3,3,1,2,3]
+	},
+	{
+	  "name":"friend4",
+	  "photo":"http://via.placeholder.com/400x400",
+	  "scores":[4,3,2,5,5,1,1,2,3,4]
+	},
+	{
+	  "name":"friend5",
+	  "photo":"http://via.placeholder.com/400x400",
+	  "scores":[3,4,3,2,1,4,5,3,2,1]
+	},
+
+];
+//this will export the firend array so the server can use it.
+module.exports = friendArray;
---
+++ server.js
@@ -0,0 +1,20 @@
+//required npm packages
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Sets up the Express App to use port 3000 or the port on Heroku
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//pulls in the routing commands from the nested folder
+//require("./routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
+
